73eb59630e0495e818d877b1770510583575ac0e
Muhammad Wajid Khan <mwajidkhan.social@gmail.com>
2023-06-20 16:18:29 +0500

    car catalogue, searchBar and custom filters

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
-import Image from 'next/image'
-import Hero from './components/Hero'
+import Image from "next/image";
+import { SearchBar, CustomFilter, Hero } from "./components";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
-      <Hero/>
+      <Hero />
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+        <div className="home__filters">
+          <SearchBar title="Search" />
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
+          </div>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
